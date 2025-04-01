e6d05249f6372784e0455c50650a931e88dd2a48
Ambrose-Kariuki <kariukiambrose093@gmail.com>
2025-04-02 00:18:50 +0300

    prime number list

--- max.py
+++ max.py
@@ -1,3 +1,8 @@
-array = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-maximum = max(array)
-print("The maximum value in the array is:", maximum)
+for num in range(2, 101):
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num, end=' ')    

b85db0aae7fa3708ebd1a13f878ad85784b66876
Ambrose-Kariuki <kariukiambrose093@gmail.com>
2025-04-02 10:26:19 +0300

    code for determining a number is even or odd

--- max.py
+++ max.py
@@ -1,9 +1,5 @@
-a = 2 
-b = 3
-print("before swap")
-print("a =", a)
-print("b =", b)
-a, b = b, a
-print("after swap")
-print ("a =", a)
-print("b =", b)
+num = int(input("Enter anumber:"))
+if num % 2 == 0:
+    print(f"Number is even: {num}")
+else:
+    print(f"Number is odd: {num}")
